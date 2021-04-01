54e99724f936dd4824ee5bf69401e39de0c84955
Gregor Lato <grzegorz.lato@gmail.com>
2021-04-02 00:29:40 +0200

    Added a renumbering based on community sizes after calculating louvain modularity, facilitating more deterministic results

--- emerge/metrics/modularity/modularity.py
+++ emerge/metrics/modularity/modularity.py
@@ -55,19 +55,21 @@ class LouvainModularityMetric(GraphMetric):
             digraph: DiGraph = graph_instance.digraph
             undirected_graph = digraph.to_undirected()
             try:
-                optimization_runs = 10
+                optimization_runs = 5
                 sum_communities_found, sum_modularity = 0, 0.0
                 sum_biggest_five_community_distribution = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0}
 
                 for run in range(optimization_runs):
 
-                    partition_by_louvain = community_louvain.best_partition(undirected_graph, resolution=1.2)
+                    partition_by_louvain = community_louvain.best_partition(undirected_graph, resolution=1.5)
+
                     communities_found = max(partition_by_louvain.values()) + 1
                     modularity = community_louvain.modularity(partition_by_louvain, undirected_graph)
 
                     sum_communities_found += communities_found
                     sum_modularity += modularity
 
+                    # sort by community size
                     community_sizes = {}
                     for i in range(communities_found):
                         community_sizes[i] = sum(map((i).__eq__, partition_by_louvain.values()))
@@ -82,22 +84,33 @@ class LouvainModularityMetric(GraphMetric):
 
                     # fetch community ids in the last iteration and write metric data
                     if run == optimization_runs - 1:
-                        for node_name in partition_by_louvain:
+
+                        # renumber key/value mappings so that increasing louvain community ids are sorted with descreasing partition sizes
+                        sorted_partion_by_louvain = {}
+
+                        # this adds more stability to the non-deterministic partition if e.g. coloring by a fixed input set of colors
+                        new_community_id = 0
+                        for sorted_community_id in sorted_community_sizes.keys():
+                            new_partition = {k: new_community_id for k, v in partition_by_louvain.items() if v == sorted_community_id}
+                            sorted_partion_by_louvain.update(new_partition)
+                            new_community_id += 1
+
+                        for node_name in sorted_partion_by_louvain:
                             if node_name in results:
                                 result = results[node_name]
                                 if isinstance(result, AbstractFileResult):
-                                    result.metrics[self.Keys.LOUVAIN_MODULARITY_IN_FILE.value] = partition_by_louvain[node_name]
-                                    self.local_data[result.unique_name] = {self.Keys.LOUVAIN_MODULARITY_IN_FILE.value: partition_by_louvain[node_name]}
+                                    result.metrics[self.Keys.LOUVAIN_MODULARITY_IN_FILE.value] = sorted_partion_by_louvain[node_name]
+                                    self.local_data[result.unique_name] = {self.Keys.LOUVAIN_MODULARITY_IN_FILE.value: sorted_partion_by_louvain[node_name]}
                                 if isinstance(result, AbstractEntityResult):
-                                    result.metrics[self.Keys.LOUVAIN_MODULARITY_IN_ENTITY.value] = partition_by_louvain[node_name]
-                                    self.local_data[result.unique_name] = {self.Keys.LOUVAIN_MODULARITY_IN_ENTITY.value: partition_by_louvain[node_name]}
+                                    result.metrics[self.Keys.LOUVAIN_MODULARITY_IN_ENTITY.value] = sorted_partion_by_louvain[node_name]
+                                    self.local_data[result.unique_name] = {self.Keys.LOUVAIN_MODULARITY_IN_ENTITY.value: sorted_partion_by_louvain[node_name]}
                             else:  # if it's not in results, it must be a dependency outside of the analysis
                                 if graph_instance.graph_type == GraphType.ENTITY_RESULT_DEPENDENCY_GRAPH or \
                                         graph_instance.graph_type == GraphType.ENTITY_RESULT_INHERITANCE_GRAPH or \
                                         graph_instance.graph_type == GraphType.ENTITY_RESULT_COMPLETE_GRAPH:
-                                    self.local_data[node_name] = {self.Keys.LOUVAIN_MODULARITY_IN_ENTITY.value: partition_by_louvain[node_name]}
+                                    self.local_data[node_name] = {self.Keys.LOUVAIN_MODULARITY_IN_ENTITY.value: sorted_partion_by_louvain[node_name]}
                                 if graph_instance.graph_type == GraphType.FILE_RESULT_DEPENDENCY_GRAPH:
-                                    self.local_data[node_name] = {self.Keys.LOUVAIN_MODULARITY_IN_FILE.value: partition_by_louvain[node_name]}
+                                    self.local_data[node_name] = {self.Keys.LOUVAIN_MODULARITY_IN_FILE.value: sorted_partion_by_louvain[node_name]}
 
                 for index, distribution in sum_biggest_five_community_distribution.items():
                     sum_biggest_five_community_distribution[index] = round((distribution / optimization_runs) / undirected_graph.number_of_nodes(), 2)
