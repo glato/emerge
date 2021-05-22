b2b53f640f107020a5f2a4135ae3f258572da95c
Gregor Lato <grzegorz.lato@gmail.com>
2021-05-22 21:15:45 +0200

    Resolve the export path of the output and web app before printing it out to console

--- emerge/analysis.py
+++ emerge/analysis.py
@@ -265,9 +265,11 @@ class Analysis:
         elif self.export_tabular_console:
             TableExporter.export_statistics_and_metrics_to_console(statistics, overall_metric_results, local_metric_results, analysis_name)
 
-        LOGGER.info_done(f'all your generated/exported data can be found here: {self.export_directory}')
+        resolved_export_path = PosixPath(self.export_directory).resolve()
+
+        LOGGER.info_done(f'all your generated/exported data can be found here: {resolved_export_path}')
         if self.export_d3:
-            LOGGER.info_done(f'you can browse your interactive web app at: file://{self.export_directory}/force-graph-html/d3-force-graph-template.html')
+            LOGGER.info_done(f'copy the following path to your browser and start your web app: file://{resolved_export_path}/force-graph-html/d3-force-graph-template.html')
 
     @property
     def entity_results(self) -> Dict[str, AbstractEntityResult]:
