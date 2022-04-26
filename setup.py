b91776dd0de48fa306e6394f51b53bd84a81c1d3
Gregor Lato <grzegorz.lato@gmail.com>
2022-04-26 22:13:46 +0200

    Bumped version to 1.2.2, only supports python 3.8+ now

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 
     install_requires=[
