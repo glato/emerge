7c2cacc94e6d546e9f9bc3a40bdaa98d0648d8bc
Gregor Lato <grzegorz.lato@gmail.com>
2023-05-12 07:31:35 +0200

    Added first contributor search implementation

--- emerge/metrics/git/git.py
+++ emerge/metrics/git/git.py
@@ -58,7 +58,7 @@ class GitMetrics(CodeMetric):
         self.file_result_prefix = ""
         self.file_result_prefix_full = ""
         
-        self.last_number_of_commits_for_calculation = 10000
+        self.last_number_of_commits_for_calculation = 6000
 
     def init(self):
         if self.analysis.source_directory and self.analysis.git_directory:
