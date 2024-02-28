beab10d95a7d35f8910442e5a448bc78bfe8f441
Mayank Maurya <mayank.maurya@proqio.com>
2024-02-28 10:35:59 +0530

    message fix

--- emerge/config.py
+++ emerge/config.py
@@ -732,7 +732,7 @@ class YamlLoader:
             with open(yaml_file_name, encoding="utf-8") as yaml_file:
                 self._config_file_content = yaml_file.read()
         except OSError:
-            LOGGER.error(f'coould not open file: {yaml_file_name}')
+            LOGGER.error(f'could not open file: {yaml_file_name}')
 
     def _load_yaml_from_config_file_content(self):
         try:
